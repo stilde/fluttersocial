48c03da76ea46f443d729b1564a2109847a890ae
sylvain <sylvain@stilde.fr>
2019-03-20 08:26:53 +0100

    third commit

--- lib/view/my_widgets/constants.dart
+++ lib/view/my_widgets/constants.dart
@@ -5,7 +5,7 @@ const Color base = const Color(0xFFBDBDBD);
 const Color baseAccent = const Color(0xFF616161);
 const Color pointer = const Color(0xFFF44336);
 
-//images
+//images dee
 
 AssetImage logoImage = AssetImage("assets\darkBee.png");
 AssetImage eventImage = AssetImage("assets\event.jpg");
